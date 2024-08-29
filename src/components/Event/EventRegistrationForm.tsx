b738745da70e3700c00faa6c965cac2e92b763c6
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-29 11:34:32 -0700

    Event reg form for members

--- src/components/Event/EventRegistrationForm.tsx
+++ src/components/Event/EventRegistrationForm.tsx
@@ -1,21 +1,36 @@
 import '../component-theme.css';
+import {useForm} from "react-hook-form";
+import {OnboardingFormSchema, UserZodObj} from "../OnboardingForm/types";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {EventRegFormSchema, EventRegZodObj} from "../FormInput/EventRegFormUtils";
+import EventRegistrationFormInput from "../FormInput/EventRegFormInput";
 export default function EventRegistrationForm() {
-    function handleSubmit() {
+    const {
+        register,
+        unregister,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<EventRegFormSchema>({
+        resolver: zodResolver(EventRegZodObj)
+    });
+
+    function submit() {
 
     }
 
     return (
-        <form autoComplete="off" onSubmit={() => handleSubmit()}>
-            <select required>
+        <form className={"form-content"} autoComplete="off" onSubmit={() => handleSubmit()}>
+            <select className={"form-select form-select-dark-blue"} required>
                 <option value={""} hidden>How familiar are you with product management?</option>
                 <option value={"beginner"}>Beginner; little to no knowledge</option>
                 <option value={"intermediate"}>Intermediate; some knowledge from competitions, courses, self research</option>
                 <option value={"advanced"}>Advanced; had formal experience, familiar with frameworks & applications</option>
                 <option value={"mentor"}>Mentor; have mentored before</option>
             </select>
-            <select required>
+            <select className={"form-select form-select-dark-blue"} required>
                 <option value={""} hidden>How did you find out about this event?</option>
-                <option value={"instagram"}>Instagram</option>
+                <option value="instagram">Instagram</option>
                 <option value="linkedin">LinkedIn</option>
                 <option value="newsletter">Newsletter</option>
                 <option value="website">Website</option>
@@ -24,8 +39,18 @@ export default function EventRegistrationForm() {
                 <option value="pmc-booth">PMC Booth</option>
                 <option value="other">Other</option>
             </select>
-            <input type={"text"} placeholder={"Dietary restrictions"}/>
-            <input type={"text"} placeholder={"What do you hope to get out of this event?"}/>
+            <EventRegistrationFormInput
+                type={"text"}
+                placeholder={"Dietary restrictions"}
+                name={"dietary"}
+                register={register}
+                error={errors.dietary}/>
+            <EventRegistrationFormInput
+                type={"text"}
+                placeholder={"What do you hope to get out of this event?"}
+                name={"expectation"}
+                register={register}
+                error={errors.expectation}/>
             <button className="pmc-button pmc-button-white" type="submit">Sign Up</button>
         </form>
     )
