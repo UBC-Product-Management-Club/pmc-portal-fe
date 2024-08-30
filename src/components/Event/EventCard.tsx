e7fc3dada4c6cde70224e6df74d7e453042188cc
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-30 14:58:02 -0700

    Fix events in dashboard

--- src/components/Event/EventCard.tsx
+++ src/components/Event/EventCard.tsx
@@ -11,40 +11,45 @@ type EventCardProps = {
 }
 
 export function EventCard(props: EventCardProps) {
-    return <div className={`card ${
-        !props.currentUser && !props.event.non_member_price ? "disabled-card" : ""
-    }`}
-                onClick={props.onClick}
-    >
-        <div className="event-date">
+    return (
+        <div>
             <h2>{new Date(props.event.date).toDateString()}</h2>
-        </div>
-
-        <div className="event">
-            <p className="event-time-loc">7:00 PM | {props.event.location}</p>
-            <h2>{props.event.name}</h2>
-            <p className="event-description">{props.event.description}</p>
-
-            <img
-                src={props.event.media[0]}
-                alt="Event"
-                className="event-image"
-            ></img>
-            {props.onRegister &&
-            <button
-                className="event-button"
-                onClick={props.onRegister}
+            <div className={`event ${
+                !props.currentUser && !props.event.non_member_price ? "disabled-card" : ""
+            }`}
+                 onClick={props.onClick}
             >
-                Register
-            </button>}
-            {!props.event.non_member_price && !props.currentUser && (
-                <div className="overlay">
-                    <p className="disabled-comment">
-                        Please sign in to your PMC account to view the details
-                        for this event.
-                    </p>
+                <div className={"card-container"}>
+                    <div className={"event-col"}>
+                        <p className="event-time-loc">7:00 PM | {props.event.location}</p>
+                        <h2>{props.event.name}</h2>
+                        <p className="event-description">{props.event.description}</p>
+                        {props.onRegister &&
+                            <button
+                                className="event-button"
+                                onClick={props.onRegister}
+                            >
+                                Register
+                            </button>}
+                        {!props.event.non_member_price && !props.currentUser && (
+                            <div className="overlay">
+                                <p className="disabled-comment">
+                                    Please sign in to your PMC account to view the details
+                                    for this event.
+                                </p>
+                            </div>
+                        )}
+                        {/*</div>*/}
+                    </div>
+                    <div className={"event-col"}>
+                        <img
+                            src={props.event.media[0]}
+                            alt="Event"
+                            className={"event-image"}
+                        ></img>
+                    </div>
                 </div>
-            )}
+            </div>
         </div>
-    </div>;
+    );
 }
