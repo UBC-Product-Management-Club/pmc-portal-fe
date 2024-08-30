06abdf6ceab03ae207cb7e450d2839aa8f4b9e5d
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-30 13:01:45 -0700

    Layout component to support fast refresh

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,9 @@ import "./index.css";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard.tsx";
 import Event from "./pages/Dashboard/Event.tsx";
-import { AuthProvider } from "./providers/Auth/AuthProvider.tsx";
+import {AuthProvider} from "./providers/Auth/AuthProvider.tsx";
 import {Profile} from "./pages/Profile/Profile";
-import {Navbar} from "./components/Navbar";
-import { Outlet } from 'react-router-dom';
-
-const Layout = () => {
-  return (
-      <div className={"width-fit"}>
-        <Navbar />
-          <div className={"container"}>
-              <Outlet />
-          </div>
-      </div>
-  );
-};
+import {Layout} from "./layout";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
