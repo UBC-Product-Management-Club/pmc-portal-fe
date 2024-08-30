b470256eb4f7b9d59c721d41bc8f91c5f5f53a24
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-30 12:50:08 -0700

    Profile page CSS + icons

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,8 +1,12 @@
+import "./Profile.css"
 import {useAuth} from "../../providers/Auth/AuthProvider";
 import {useState} from "react";
 import {EventCard} from "../../components/Event/EventCard";
 import {eventType} from "../../types/api";
 import {useNavigate} from "react-router-dom";
+import {FiBook} from "react-icons/fi";
+import {MdOutlineEdit} from "react-icons/md";
+import {TbSchool} from "react-icons/tb";
 
 export function Profile() {
     const {currentUser} = useAuth();
@@ -10,28 +14,39 @@ export function Profile() {
     const [events] = useState<eventType[]>([]);
 
     return (
-        <div>
-            <div className={"profile-row"}>
-                <img/>
-                <h2>Name</h2>
-                <div className={"profile-pill"}>
-                    <img/>
-                    <p>University</p>
+        <div className={"profile"}>
+            <div className={"profile-space-around"}>
+                <div className={"profile-picture-wrapper w-50"}>
+                <img className={"profile-picture"}
+                    src={"https://i.natgeofe.com/n/23a85f5b-32be-4bc1-af13-3e1a403b8557/mountain-gorilla_thumb_square.jpg"}
+                    alt={"Profile Picture"}/>
                 </div>
-                <div className={"profile-pill"}>
-                    <img/>
-                    <p>Year, Faculty, Major</p>
+                <div className={"w-50"}>
+                    <div className={"profile-name-pronouns"}>
+                        <h2>Name</h2>
+                        <p>(pronouns)</p>
+                    </div>
+                    <div className={"profile-pill"}>
+                        <TbSchool/>
+                        <p>University</p>
+                    </div>
+                    <div className={"profile-pill"}>
+                        <FiBook/>
+                        <p>Year, Faculty, Major</p>
+                    </div>
                 </div>
             </div>
 
-            <div className={"profile-row"}>
+            <div className={"profile-space-between"}>
                 <h3>Why Product Management?</h3>
-                <div className={"profile-pill-sm"}>
+                <div className={"profile-pill"}>
                     <p>Edit</p>
-                    <img/>
+                    <MdOutlineEdit/>
                 </div>
-                <p className={"profile-why-pm"}></p>
             </div>
+            <p className={"profile-why-pm"}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
 
             <div>
                 <h3>Events Registered</h3>
