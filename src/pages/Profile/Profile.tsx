22d199375a019650505a3d84bb6c9ef8864b369c
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-30 13:58:07 -0700

    Edit why PM

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import "./Profile.css"
 import {useAuth} from "../../providers/Auth/AuthProvider";
-import {useState} from "react";
+import {ChangeEvent, Dispatch, SetStateAction, useState} from "react";
 import {EventCard} from "../../components/Event/EventCard";
 import {eventType} from "../../types/api";
 import {useNavigate} from "react-router-dom";
@@ -8,18 +8,44 @@ import {FiBook} from "react-icons/fi";
 import {MdOutlineEdit} from "react-icons/md";
 import {TbSchool} from "react-icons/tb";
 
+function ProfileWhyPM(props: { text: string, setText: Dispatch<SetStateAction<string>> }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        props.setText(event.target.value);
+    }
+
+    return <div>
+        <div className={"profile-space-between"}>
+            <h3>Why Product Management?</h3>
+            <button onClick={() => setIsEditing(!isEditing)} className={"profile-pill"}>
+                <p>{isEditing ? 'Save' : 'Edit'}</p>
+                <MdOutlineEdit/>
+            </button>
+        </div>
+        <textarea className={"profile-why-pm"}
+            value={props.text}
+            onChange={handleChange}
+            readOnly={!isEditing}
+            rows={4}
+            cols={50}
+        />
+    </div>;
+}
+
 export function Profile() {
     const {currentUser} = useAuth();
     const navigateTo = useNavigate();
     const [events] = useState<eventType[]>([]);
+    const [whyPM, setWhyPM] = useState("Why are you interested in Product Management…");
 
     return (
         <div className={"profile"}>
             <div className={"profile-space-around"}>
                 <div className={"profile-picture-wrapper w-50"}>
-                <img className={"profile-picture"}
-                    src={"https://i.natgeofe.com/n/23a85f5b-32be-4bc1-af13-3e1a403b8557/mountain-gorilla_thumb_square.jpg"}
-                    alt={"Profile Picture"}/>
+                    <img className={"profile-picture"}
+                         src={"https://i.natgeofe.com/n/23a85f5b-32be-4bc1-af13-3e1a403b8557/mountain-gorilla_thumb_square.jpg"}
+                         alt={"Profile Picture"}/>
                 </div>
                 <div className={"w-50"}>
                     <div className={"profile-name-pronouns"}>
@@ -27,9 +53,9 @@ export function Profile() {
                         <p>(pronouns)</p>
                     </div>
                     <div className={"profile-pill"}>
-                        <TbSchool/>
-                        <p>University</p>
-                    </div>
+                            <TbSchool/>
+                            <p>University</p>
+                        </div>
                     <div className={"profile-pill"}>
                         <FiBook/>
                         <p>Year, Faculty, Major</p>
@@ -37,16 +63,7 @@ export function Profile() {
                 </div>
             </div>
 
-            <div className={"profile-space-between"}>
-                <h3>Why Product Management?</h3>
-                <div className={"profile-pill"}>
-                    <p>Edit</p>
-                    <MdOutlineEdit/>
-                </div>
-            </div>
-            <p className={"profile-why-pm"}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+            <ProfileWhyPM text={whyPM} setText={setWhyPM}/>
 
             <div>
                 <h3>Events Registered</h3>
@@ -56,12 +73,14 @@ export function Profile() {
                             key={event.event_Id}
                             currentUser={currentUser}
                             event={event}
-                            onClick={() => {navigateTo(`/events/${event.event_Id}`);}}
+                            onClick={() => {
+                                navigateTo(`/events/${event.event_Id}`);
+                            }}
                             onRegister={(e) => e.stopPropagation()}
                         />
                     ))
                 ) : (
-                    <p style={{ color: "white" }}>No events found.</p>
+                    <p style={{color: "white"}}>No events found.</p>
                 )}
             </div>
         </div>
