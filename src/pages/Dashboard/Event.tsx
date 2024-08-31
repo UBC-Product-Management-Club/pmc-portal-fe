ae1be32a61870e8d37b90ad05d0f807b06c057fa
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-30 19:25:19 -0700

    Bring back member/non member price, fix merge

--- src/pages/Dashboard/Event.tsx
+++ src/pages/Dashboard/Event.tsx
@@ -138,7 +138,9 @@ const Event: React.FC = () => {
       <EventRegistrationModal
           isModalOpen={isSignUpFormOpen}
           setIsModalOpen={setIsSignUpFormOpen}
-          eventId={event_id ?? ""}/>
+          eventId={event_id ?? ""}
+          memberPrice={event.member_price}
+          nonMemberPrice={event.non_member_price}/>
 
       <img src={event.media[0]} alt="Event" className="event-photo"></img>
       <div className="event-desc">
