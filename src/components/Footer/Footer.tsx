ed71f5e17112814dd936385155d187dcb56e2f97
Toby <toby.lau075@gmail.com>
2024-09-06 08:24:49 -0700

    Created and added footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <div className="footer-container">
+            <div className="footer-content">
+                <p>Need help? Contact <a href="mailto:tech@ubcpmc.com">tech@ubcpmc.com</a> for support.</p>
+            </div>
+        </div>
+    )
+}
