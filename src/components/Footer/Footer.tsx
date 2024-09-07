43148bf9c1a19f2b7585934ba2d3a0f2d120ad17
Toby <toby.lau075@gmail.com>
2024-09-07 12:12:48 -0700

    Added a footer. Needs testing to make sure it doesn't screw up the layout with banner and events.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
+import "./Footer.css"
 
 export default function Footer() {
     return (
         <div className="footer-container">
             <div className="footer-content">
-                <p>Need help? Contact <a href="mailto:tech@ubcpmc.com">tech@ubcpmc.com</a> for support.</p>
+                <p className="footer-text">Need help? Contact <a className="footer-link" href="mailto:tech@ubcpmc.com">tech@ubcpmc.com</a> for support.</p>
             </div>
         </div>
     )
