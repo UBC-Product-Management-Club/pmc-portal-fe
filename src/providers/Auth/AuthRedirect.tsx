dbffad410c88886fa33df5646737bea35352c54d
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-10-08 14:59:48 -0700

    Remove console logs

--- src/providers/Auth/AuthRedirect.tsx
+++ src/providers/Auth/AuthRedirect.tsx
@@ -4,21 +4,17 @@ import {useAuth0} from "@auth0/auth0-react";
 import {useAuth} from "./AuthProvider";
 
 export default function AuthRedirect() {
-    const {user, isAuthenticated, isLoading} = useAuth0();
+    const {isAuthenticated, isLoading} = useAuth0();
     const {userData} = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!isLoading) {
             if (!isAuthenticated) {
-                console.log(user)
-                console.log(isAuthenticated)
                 navigate("/login");
             } else if (isAuthenticated && (!userData || !userData.onboarded)) {
-                console.log("onboarding")
                 navigate("/onboarding");
             } else if (isAuthenticated && userData && userData.onboarded) {
-                console.log("dashboard")
                 navigate("/dashboard");
             }
         }
