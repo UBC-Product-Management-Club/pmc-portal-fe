c4b3309eab0cb2e470d8c7aae286e21cf4554c7a
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-11-06 17:29:41 -0800

    fix falsy statement to check if non member price exists

--- src/components/Event/EventCard.tsx
+++ src/components/Event/EventCard.tsx
@@ -1,66 +1,66 @@
 import "./EventCard.css";
-import { eventType } from "../../types/api";
-import { useNavigate } from "react-router-dom";
+import {eventType} from "../../types/api";
+import {useNavigate} from "react-router-dom";
 
 type EventCardProps = {
-  isSignedIn: boolean;
-  event: eventType;
-  showRegister?: boolean;
+    isSignedIn: boolean;
+    event: eventType;
+    showRegister?: boolean;
 };
 
 export function EventCard(props: EventCardProps) {
-  const navigateTo = useNavigate()
+    const navigateTo = useNavigate()
 
-  return (
-    <div>
-      <h2>{new Date(props.event.date).toDateString()}</h2>
-      <div
-        className={`event ${
-          !props.isSignedIn && !props.event.non_member_price
-            ? "disabled-card"
-            : ""
-        }`}
-      >
-        <div className={"card-container"}>
-          <div className={"event-col"}>
-            <p className="event-time-loc">7:00 PM | {props.event.location}</p>
-            <p className="event-name">{props.event.name}</p>
-            <p className="event-description">{props.event.description}</p>
+    return (
+        <div>
+            <h2>{new Date(props.event.date).toDateString()}</h2>
+            <div
+                className={`event ${
+                    !props.isSignedIn && props.event.non_member_price === undefined
+                        ? "disabled-card"
+                        : ""
+                }`}
+            >
+                <div className={"card-container"}>
+                    <div className={"event-col"}>
+                        <p className="event-time-loc">7:00 PM | {props.event.location}</p>
+                        <p className="event-name">{props.event.name}</p>
+                        <p className="event-description">{props.event.description}</p>
 
-            {props.showRegister && (
-              <button
-                className={`event-button ${props.event.maxAttendee !== null &&
-                  props.event.attendee_Ids?.length >= props.event.maxAttendee
-                  ? "disabled-button"
-                  : ""
-                  }`}
-                onClick={() => {
-                  navigateTo(`/events/${props.event.event_Id}`);
-                }
-                }
+                        {props.showRegister && (
+                            <button
+                                className={`event-button ${props.event.maxAttendee !== null &&
+                                props.event.attendee_Ids?.length >= props.event.maxAttendee
+                                    ? "disabled-button"
+                                    : ""
+                                }`}
+                                onClick={() => {
+                                    navigateTo(`/events/${props.event.event_Id}`);
+                                }
+                                }
 
-              >
-                See more
-              </button>
-            )}
-            {!props.event.non_member_price && !props.isSignedIn && (
-              <div className="overlay">
-                <p className="disabled-comment">
-                  Please sign in to your PMC account to view the details for
-                  this event.
-                </p>
-              </div>
-            )}
-          </div>
-          <div className={"event-col"}>
-            <img
-              src={props.event.media[0]}
-              alt="Event"
-              className={"event-image"}
-            ></img>
-          </div>
+                            >
+                                See more
+                            </button>
+                        )}
+                        {props.event.non_member_price === undefined && !props.isSignedIn && (
+                            <div className="overlay">
+                                <p className="disabled-comment">
+                                    Please sign in to your PMC account to view the details for
+                                    this event.
+                                </p>
+                            </div>
+                        )}
+                    </div>
+                    <div className={"event-col"}>
+                        <img
+                            src={props.event.media[0]}
+                            alt="Event"
+                            className={"event-image"}
+                        ></img>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
