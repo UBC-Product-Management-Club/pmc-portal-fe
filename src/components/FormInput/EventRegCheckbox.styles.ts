d10feb3f8ff576461577ca47a333ac7c717f15df
Gordon Cheung <gocheung@macbook-pro.lan>
2025-01-02 16:11:15 -0800

    implement checkbox

---
+++ src/components/FormInput/EventRegCheckbox.styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const CheckboxGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const CheckboxLabel = styled.label`
+  font-size: 14px;
+  font-weight: 500;
+  color: #FFF;
+  margin-bottom: 8px;
+`;
+
+export const CheckboxItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const CheckboxInput = styled.input`
+  cursor: pointer;
+  width: 18px;
+  height: 18px;
+  accent-color: #5C5CFF;
+`;
+
+export const OptionLabel = styled.label`
+  cursor: pointer;
+  color: #FFF;
+  font-size: 14px;
+`; 
