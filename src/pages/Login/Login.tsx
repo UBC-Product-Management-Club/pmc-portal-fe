7f4b7e965ed869940cdaf20576f5e76b8dc24719
Gordon Cheung <gocheung@macbook-pro.lan>
2025-01-09 18:11:29 -0800

    add in-app browser redirect

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,36 @@
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import PMCLogo from "../../assets/pmclogo.svg";
-import {useAuth0} from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import Footer from "../../components/Footer/Footer";
 
+// Helper function to detect in-app browsers
+const isInAppBrowser = () => {
+  const ua = window.navigator.userAgent.toLowerCase();
+  const isIOS = /iphone|ipad|ipod/.test(ua);
+  return (
+    ua.includes('wv') || // Android WebView
+    (isIOS && !ua.includes('safari')) || // iOS WebView
+    ua.includes('fbav') || // Facebook
+    ua.includes('instagram') || // Instagram
+    ua.includes('twitter') || // X (formerly Twitter)
+    ua.includes('x-client') || // X's new client identifier
+    ua.includes('linkedin') // LinkedIn
+  );
+};
+
 export default function Login() {
-  const {loginWithRedirect} = useAuth0();
+  const { loginWithRedirect } = useAuth0();
   const navigateTo = useNavigate();
+
+  const handleLogin = () => {
+    if (isInAppBrowser()) {
+      // Open in default browser
+      window.open(window.location.href, '_system');
+    } else {
+      loginWithRedirect();
+    }
+  };
 
   return (
     <div className="login-container">
@@ -14,7 +38,7 @@ export default function Login() {
         <img className="login-content--logo" src={PMCLogo} />
         <h1 className="login-content--header">PMC Membership Portal</h1>
         <div className="login-content--button-container">
-          <button className="login-googlesso" onClick={() => loginWithRedirect()}>
+          <button className="login-googlesso" onClick={handleLogin}>
             Log in / sign up
           </button>
           <button
