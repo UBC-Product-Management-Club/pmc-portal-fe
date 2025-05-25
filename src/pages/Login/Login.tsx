95af62b300206511715d12ccd8e0e1a5d128a5b8
Kevin Lei <kevinleimc@gmail.com>
2025-05-25 13:13:07 -0700

    refactor login component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,6 @@
-import "./Login.css";
 import PMCLogo from "../../assets/pmclogo.svg";
 import Footer from "../../components/Footer/Footer";
-
+import styled from 'styled-components'
 // Helper function to detect in-app browsers
 // const isInAppBrowser = () => {
 //   const ua = window.navigator.userAgent.toLowerCase();
@@ -34,26 +33,149 @@ export default function Login() {
 //     }
 //   };
 
+
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    width: 100vw;
+    height: 100vh;
+    background-color: var(--pmc-blue);
+    position: relative;
+    @media screen and (max-width: 768px) {
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      background-color: var(--pmc-dark-blue);
+    }
+  `;
+
+
+  const Content = styled.div`  
+    width: 36rem;
+    height: 27rem;
+    margin: auto;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 1rem;
+    background-color: var(--pmc-dark-blue);
+    @media screen and (max-width: 768px) {
+      width: 100%;
+      height: 100;
+      border-radius: 0rem;
+    }
+  `;
+
+
+  const Logo = styled.img`
+    width: 10rem;
+    height: 10rem;
+    @media screen and (max-width: 768px) {
+      width: 8rem;
+      height: 8rem;
+    }
+  `;
+
+
+  const Header = styled.h1`  
+    text-align: center;
+    font-family: Helvetica;
+    line-height: 1.5;
+    background: #fff;
+    color: transparent;
+    background-clip: text;
+    @media screen and (max-width: 768px) {
+      text-align: center;
+      font-size: x-large;
+      font-family: Helvetica;
+      line-height: 1.5;
+      background: #fff;
+      color: transparent;
+      background-clip: text;
+    }
+  `;
+
+
+  const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+
+    > button {
+      width: 300px;
+      padding: 0.75rem 0;
+      margin: 0.5rem 0;
+      border-radius: 999rem;
+      cursor: pointer;
+    }
+    @media screen and (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+      > button {
+        width: 300px;
+        padding: 0.75rem 0;
+        margin: 0.5rem 0;
+        border-radius: 999rem;
+        cursor: pointer;
+      }
+    }
+  `;
+
+
+  const LoginButton = styled.button`
+    font-weight: 400;
+    display: inline-flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: medium;
+    color: #1c1c1c;
+    background-color: white;
+    border: 0;
+  `;
+
+
+  const ContinueButton = styled.button`
+    background: transparent;
+    border: solid;
+    border-width: 0.2rem;
+    border-color: white;
+    color: white;
+    font-size: medium;
+  `;
+
+
+  const FooterContainer = styled.div`
+    width: 100%;
+    position: absolute;
+    bottom: 1rem;
+  `;
+ 
   return (
-    <div className="login-container">
-      <div className="login-content">
-        <img className="login-content--logo" data-testid="logo" src={PMCLogo} />
-        <h1 className="login-content--header">PMC Membership Portal</h1>
-        <div className="login-content--button-container">
-          <button className="login-googlesso" onClick={() => console.log("under construction!")}>
+    <Container>
+      <Content>
+        <Logo src={PMCLogo} data-testid="logo" alt="PMC Logo"/>
+        <Header> PMC Membership Portal</ Header>
+        <ButtonContainer>
+          <LoginButton onClick={() => {}}>
             Log in / sign up
-          </button>
-          <button
-            className="login-continue"
-            onClick={() => console.log("under construction!")}
+          </LoginButton>
+          <ContinueButton
+            onClick={() => {}}
           >
             Continue as a non-member
-          </button>
-        </div>
-      </div>
-      <div className="login-footer--container">
+          </ContinueButton>
+        </ButtonContainer>
+      </Content>
+      <FooterContainer>
         <Footer />
-      </div>
-    </div>
+      </FooterContainer>
+    </Container>
   );
 }
