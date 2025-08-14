42bfdf17aa9c2cc500f356182aa37b73b2c26900
Jeffrey <jzhai08@students.cs.ubc.ca>
2025-08-13 17:01:17 -0700

    reverting a change made to event service

--- src/service/EventService.ts
+++ src/service/EventService.ts
@@ -6,8 +6,7 @@ class EventService {
     private client: RestClient;
 
     constructor(client?: RestClient) {
-        //this.client = client ?? new RestClient(`${import.meta.env.VITE_API_URL}/api/v2/events`);
-        this.client = client ?? new RestClient(`http://localhost:8000/api/v2/events`);
+        this.client = client ?? new RestClient(`${import.meta.env.VITE_API_URL}/api/v2/events`);
     }
 
     getAll(): Promise<EventCard[]> {
