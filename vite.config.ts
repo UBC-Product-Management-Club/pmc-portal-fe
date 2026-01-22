264aad5c129c3eddc10175fa95fe714860f3045d
gonsis10 <simoonsong1006@gmail.com>
2026-01-21 22:29:54 -0800

    feat: added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: ".secret",
-  plugins: [react()],
-  test: {
-    globals: true,
-    setupFiles: "src/config/testSetup",
-    environment: "jsdom"
-  },
-})
+    envDir: '.secret',
+    plugins: [react(), tailwindcss()],
+    test: {
+        globals: true,
+        setupFiles: 'src/config/testSetup',
+        environment: 'jsdom',
+    },
+});
